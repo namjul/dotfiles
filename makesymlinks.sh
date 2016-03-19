a3ab04b1b26a18177b4f12f6423354ae9c29ca70
samho <nam@hobl.at>
2016-03-19 19:43:51 +0100

    switch dotfiles path to ~/.dotfiles

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -7,8 +7,8 @@
 
 ########## Variables
 
-dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
+dir=~/.dotfiles                    # dotfiles directory
+olddir=~/.dotfiles_old             # old dotfiles backup directory
 files="bashrc bash_profile gitconfig vimrc vim inputrc tmux.conf"    # list of files/folders to symlink in homedir
 
 ##########
