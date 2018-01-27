3b0afc594a588d66fbdec499ecbd195e9fa51ff8
Samuel Hobl <nam@hobl.at>
2018-01-27 14:20:08 +0100

    Update xmonad config

--- xmonad/dot.xmonad.hs
+++ xmonad/dot.xmonad.hs
@@ -14,13 +14,19 @@ main = do
   xmproc <- spawnPipe "xmobar"
   xmonad $ defaultConfig
         { manageHook = manageDocks <+> myManageHook <+> manageHook defaultConfig
-        , layoutHook = avoidStruts  $  layoutHook defaultConfig
+        , layoutHook = avoidStruts $ layoutHook defaultConfig
+        , terminal = "gnome-terminal"
         , logHook = dynamicLogWithPP xmobarPP
                         { ppOutput = hPutStrLn xmproc
                         , ppTitle = xmobarColor "green" "" . shorten 50
+                        , ppCurrent = xmobarColor "#fff" "" . wrap "[" "]" 
                         }
-        } `additionalKeys`
+        } `additionalKeys` myAdditionalKeys
+
+myAdditionalKeys =
         [ ((mod1Mask .|. controlMask, xK_l), spawn "i3lock -d -c 000000")
         , ((controlMask, xK_Print), spawn "shutter -s")
         , ((0, xK_Print), spawn "scrot")
+        , ((mod1Mask, xK_b), sendMessage ToggleStruts)
+        , ((mod1Mask, xK_p), spawn  "rofi -matching fuzzy -modi combi -show combi -combi-modi run,drun")
         ]
