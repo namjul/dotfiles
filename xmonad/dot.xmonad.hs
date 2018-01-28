428b9e97ebc4ecb0773b9865b13755a54d48d682
Samuel Hobl <nam@hobl.at>
2018-01-28 20:50:46 +0100

    Update xmonad config

--- xmonad/dot.xmonad.hs
+++ xmonad/dot.xmonad.hs
@@ -5,24 +5,6 @@ import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 
-myManageHook = composeAll
-    [ className =? "Gimp"      --> doFloat
-    , className =? "Vncviewer" --> doFloat
-    ]
-
-main = do
-  xmproc <- spawnPipe "xmobar"
-  xmonad $ defaultConfig
-        { manageHook = manageDocks <+> myManageHook <+> manageHook defaultConfig
-        , layoutHook = avoidStruts $ layoutHook defaultConfig
-        , terminal = "gnome-terminal"
-        , logHook = dynamicLogWithPP xmobarPP
-                        { ppOutput = hPutStrLn xmproc
-                        , ppTitle = xmobarColor "green" "" . shorten 50
-                        , ppCurrent = xmobarColor "#fff" "" . wrap "[" "]" 
-                        }
-        } `additionalKeys` myAdditionalKeys
-
 myAdditionalKeys =
         [ ((mod1Mask .|. controlMask, xK_l), spawn "i3lock -d -c 000000")
         , ((controlMask, xK_Print), spawn "shutter -s")
@@ -30,3 +12,21 @@ myAdditionalKeys =
         , ((mod1Mask, xK_b), sendMessage ToggleStruts)
         , ((mod1Mask, xK_p), spawn  "rofi -matching fuzzy -modi combi -show combi -combi-modi run,drun")
         ]
+
+myConfig p = def 
+        { manageHook = manageDocks <+> manageHook defaultConfig
+        , layoutHook = avoidStruts $ layoutHook defaultConfig
+        , terminal = "gnome-terminal"
+        , logHook = dynamicLogWithPP xmobarPP
+                        { ppOutput = hPutStrLn p
+                        , ppTitle = xmobarColor "green" "" . shorten 50
+                        , ppCurrent = xmobarColor "#fff" "" . wrap "[" "]" 
+                        }
+        , borderWidth = 1
+        , normalBorderColor = "#1d1f21"
+        , focusedBorderColor = "#8abeb7"
+        } `additionalKeys` myAdditionalKeys
+
+main = do
+  xmproc <- spawnPipe "xmobar"
+  xmonad $ myConfig xmproc
