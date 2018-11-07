075ba6bc83d7bbfc27d4d5659e2c4cbe0fcbb078
Samuel Hobl <nam@hobl.at>
2018-11-07 14:46:55 +0100

    Add linuxbrew to PATH

--- shell/path.sh
+++ shell/path.sh
@@ -3,3 +3,4 @@
 [ -d ~/.dotfiles/bin ] && prepend-path ~/.dotfiles/bin
 [ -d ~/bin ] && prepend-path ~/bin
 [ -d ~/.cargo/bin ] && prepend-path ~/.cargo/bin
+[ -d /home/linuxbrew/.linuxbrew ] && prepend-path /home/linuxbrew/.linuxbrew/bin
