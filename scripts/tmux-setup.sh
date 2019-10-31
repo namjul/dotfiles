10efc35ea4a9b15f8ba813c939bbe80b92d40f4e
Samuel Hobl <nam@hobl.at>
2019-10-31 10:57:55 +0100

    update `tmux-setup.sh`

--- scripts/tmux-setup.sh
+++ scripts/tmux-setup.sh
@@ -15,21 +15,14 @@ if [ "$?" -eq 1 ] ; then
   set -- $(stty size) # $1 = rows $2 = columns
   tmux new-session -d -s "$SESSION_NAME" -x "$2" -y "$(($1 - 1))" -n 'code' 
   tmux new-window -t "$SESSION_NAME" -n 'bash'
-  tmux new-window -t "$SESSION_NAME" -n 'notes'
   tmux new-window -t "$SESSION_NAME" -n 'dotfiles'
 
   tmux select-window -t "$SESSION_NAME:4"
   tmux send-keys "cdd" Enter
   tmux send-keys "clear" Enter
 
-  tmux select-window -t "$SESSION_NAME:3"
-  tmux send-keys "dnote view" Enter
-
-  tmux select-window -t "$SESSION_NAME:2"
-  tmux split-window -h
-
   tmux select-window -t "$SESSION_NAME:1"
-  tmux send-keys "nvim" Enter
+  tmux send-keys "nvim -c \"Gist -l\"" Enter
 fi
 
 # detach from a tmux session if in one
