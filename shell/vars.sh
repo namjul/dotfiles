cc5867185261c671381fc067571c8445120e7b90
Samuel Hobl <nam@hobl.at>
2020-02-24 13:09:29 +0100

    fix: remove unnecessary space

--- shell/vars.sh
+++ shell/vars.sh
@@ -21,5 +21,5 @@ export SHELL_DOTFILES="${DOTFILES}/shell"
 export TODOTXT_CFG_FILE="$HOME/.todo.cfg"
 
 # fzf
-export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --no-ignore --exclude  .git'
+export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --no-ignore --exclude .git'
 export FZF_DEFAULT_OPTS='--height 40% --layout=reverse --inline-info --border'
