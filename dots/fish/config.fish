1dd3c4d0a6f2c48fc8b1c32aaa906e9499d3c487
Samuel Hobl <nam@hobl.at>
2020-10-07 20:54:16 +0200

    feat(fish): make `config.fish` work in fish default shell env

--- dots/fish/config.fish
+++ dots/fish/config.fish
@@ -7,23 +7,13 @@
 #  ╚═════╝ ╚═════╝ ╚═╝  ╚═══╝╚═╝     ╚═╝ ╚═════╝ ╚═╝╚═╝     ╚═╝╚══════╝╚═╝  ╚═╝
 #
 
-
-# check if brew command exists
-if ! type -q brew
-  echo "Make sure `brew` command is available."
-  exit
+if status is-login
+  bass source ~/.profile
 end
 
-if status is-login
-  # Initialize Homebrew
-  # test -e ~/.linuxbrew && eval (~/.linuxbrew/bin/brew shellenv)
-  # test -e /home/linuxbrew/.linuxbrew/bin/brew && eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-
-  # Initialize asdf
-  # source (brew --prefix asdf)/asdf.fish
-
-  # Local settings
-  # test -e $HOME/.localrc && source $HOME/.localrc
+# notify if brew command does not exists
+if ! type -q brew
+  echo "Make sure `brew` command is available."
 end
 
 if status is-interactive
@@ -38,18 +28,13 @@ if status is-interactive
     end
   end
 
-  # if test -e ~/.localrc
-  #   source ~/.localrc
-  # end
-
   # Theme
   theme_gruvbox 'dark' 'soft'
   set -g fish_color_command "#ebdbb2" # the color for commands
 
-
   # Tmux
   if command -v tmux > /dev/null 2>&1
-     # test -z $TMUX && tmux;
+     test -z $TMUX && tmux;
   end
 
   starship init fish | source
