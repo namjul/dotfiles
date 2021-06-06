d440428b07ae2ec961e953d4d192f34a5a95814f
Samuel Hobl <nam@hobl.at>
2021-06-06 16:13:45 +0200

    feat(fish): adjust open keybindings
    one for editing file in $EDITOR
    other for opening `lf` file manager

--- dots/.config/fish/functions/fish_user_key_bindings.fish
+++ dots/.config/fish/functions/fish_user_key_bindings.fish
@@ -7,15 +7,15 @@ function fish_user_key_bindings
   bind \cf 'fzf-file-widget'
   bind \cr 'fzf-history-widget'
   bind \ec 'fzf-cd-widget'
-  bind \ce 'fdo'
+  bind \co 'fdo'
   bind --mode insert \cf 'fzf-file-widget'
   bind --mode insert \cr 'fzf-history-widget'
   bind --mode insert \ec 'fzf-cd-widget'
-  bind --mode insert \ce 'fdo'
+  bind --mode insert \co 'fdo'
 
   # lf file manager
-  bind \co 'set old_tty (stty -g); stty sane; lfcd; stty $old_tty; commandline -f repaint'
-  bind --mode insert \co 'set old_tty (stty -g); stty sane; lfcd; stty $old_tty; commandline -f repaint'
+  bind \eo 'set old_tty (stty -g); stty sane; lfcd; stty $old_tty; commandline -f repaint'
+  bind --mode insert \eo 'set old_tty (stty -g); stty sane; lfcd; stty $old_tty; commandline -f repaint'
 
 end
 
