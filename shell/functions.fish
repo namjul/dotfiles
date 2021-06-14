eacc61bea310effb7e25232707deae2d98960fbb
Samuel Hobl <nam@hobl.at>
2021-06-14 12:55:26 +0200

    enhance: `dsync` show changes and ask for confirmation before sync

--- shell/functions.fish
+++ shell/functions.fish
@@ -37,7 +37,22 @@ end
 
 # Source: https://coderwall.com/p/grmruq/git-status-on-all-repos-in-folder
 function repos --description="Git status on all repos in folder"
-  find . -maxdepth 1 -mindepth 1 -type d -exec sh -c '(echo {} && cd {} && git status -s && echo)' \;
+  find . -maxdepth 1 -mindepth 1 -type d -exec sh -c '(cd {} && [ -d .git ] && echo {} && git status -s && echo)' \;
+end
+
+function dsync --description="All dendron sync in current director"
+  if type -q dendron
+    repos
+    read -l -P 'Do you want to continue? [y/N] ' confirm
+    switch $confirm
+      case Y y
+        dendron workspace --wsRoot . sync
+      case '' N n
+        return 1
+    end
+  else
+    echo "Make sure \`dendron\` command is available."
+  end
 end
 
 # GIT heart FZF
