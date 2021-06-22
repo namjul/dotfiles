4b11a09bd0697d434cf8a056193aa034d9fdc960
Samuel Hobl <nam@hobl.at>
2021-06-22 11:27:40 +0200

    fix(neovim): allow autpairs in markdown files but exclude single `[`
    I want to use markdown snippets that conflict with this pair. I added `[[` to autoclose for wikilinks.

--- dots/.config/nvim/init.lua
+++ dots/.config/nvim/init.lua
@@ -276,7 +276,7 @@ end
 util.createAugroup({
   { 'BufRead,BufNewFile', '*.json', 'set', 'filetype=jsonc' },
   { 'FileType', 'markdown', 'lua', 'require"namjul.autocmds".plainText()' },
-  { 'FileType', 'markdown', 'let b:autopairs_enabled=0' }
+  { 'FileType', 'markdown', "let b:AutoPairs={ '(':')', '[[':']]', '{':'}', \"'\":\"'\", '\"':'\"', \"`\":\"`\", '```':'```', '\"\"\"':'\"\"\"', \"'''\":\"'''\" }" }
 }, 'namjulfiletypedetect')
 
 util.createAugroup({
