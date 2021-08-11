b4bcd67afbea305d56525f5df10d144d1fc7ce71
Samuel Hobl <nam@hobl.at>
2021-08-11 11:53:32 +0200

    fix(nvim): adjust depricated commands

--- dots/.config/nvim/init.lua
+++ dots/.config/nvim/init.lua
@@ -455,15 +455,15 @@ end
 
 if hasPlugin('vim-fugitive') then
   opt.g({ diffopt = opt.g('diffopt') .. ',vertical' })
-  map.g('n', '<leader>gb', ':Gblame<CR>')
-  map.g('n', '<leader>gs', ':Gstatus<CR>')
+  map.g('n', '<leader>gb', ':G blame<CR>')
+  map.g('n', '<leader>gs', ':Git<CR>')
   map.g('n', '<leader>gc', ':Git commit -v<CR>')
   map.g('n', '<leader>ga', ':Git add -p<CR>')
   map.g('n', '<leader>gm', ':Git commit --amend<CR>')
   map.g('n', '<leader>gp', ':Git push<CR>')
   map.g('n', '<leader>gd', ':Gdiff<CR>')
   map.g('n', '<leader>gw', ':Gwrite<CR>')
-  map.g('n', '<leader>gbr', ':Gbrowse<CR>')
+  map.g('n', '<leader>gbr', ':GBrowse<CR>')
 end
 
 if hasPlugin('vim-flog') then
