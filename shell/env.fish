484f86acf3111fab144ab226b0af89b231ba207f
Samuel Hobl <nam@hobl.at>
2021-08-13 16:32:59 +0200

    fix(fzf): correct typo

--- shell/env.fish
+++ shell/env.fish
@@ -14,7 +14,7 @@ export MANPAGER="less -X"
 export TODOTXT_CFG_FILE="$HOME/.todo.cfg"
 
 # fzf
-export FZF_DEFAULT_COMMAND='rg --files --follow --no-ignore-vcs" 2> /dev/null'
+export FZF_DEFAULT_COMMAND='rg --files --follow --no-ignore-vcs 2> /dev/null'
 export FZF_DEFAULT_OPTS='--cycle --layout=reverse --border --height 75% --preview-window=wrap'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_ALT_C_COMMAND="$FZF_DEFAULT_COMMAND --null | xargs -0 dirname | uniq"
