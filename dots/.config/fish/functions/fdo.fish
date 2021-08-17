f590dc066fe462700d23031469d51be9720cf4e3
Samuel Hobl <nam@hobl.at>
2021-08-17 17:36:27 +0200

    enhance: `fdo` command should clear commandline
    When closing editor the commandline was left in an unfished state
    (cursor was not in the next line).

--- dots/.config/fish/functions/fdo.fish
+++ dots/.config/fish/functions/fdo.fish
@@ -1,8 +1,10 @@
 
 function fdo --description "Fuzzy find & vim"
+  # check if fzf was aborted
   if test (count $argv) -gt 0
     command $EDITOR $argv
   else
     fzf -m | xargs -o $EDITOR
   end
+  commandline -f repaint
 end
