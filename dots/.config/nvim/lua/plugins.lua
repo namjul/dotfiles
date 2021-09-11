c7c31bc532733ef44ab8dfba937b785e07f69cfa
Samuel Hobl <nam@hobl.at>
2021-09-11 19:44:30 +0200

    chore(nvim): remove comment

--- dots/.config/nvim/lua/plugins.lua
+++ dots/.config/nvim/lua/plugins.lua
@@ -109,11 +109,7 @@ return require('packer').startup(function(use)
   use({
     'folke/zen-mode.nvim',
     config = function()
-      require('zen-mode').setup({
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
-      })
+      require('zen-mode').setup({})
     end,
   })
   use('szw/vim-maximizer')
