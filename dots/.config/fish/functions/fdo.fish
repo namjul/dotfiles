4936d2aa520f6ec2f459a237ec4dc019761846c7
Samuel Hobl <nam@hobl.at>
2021-10-24 14:43:51 +0200

    feat(fish): open pdf not with editor but with preferred application

--- dots/.config/fish/functions/fdo.fish
+++ dots/.config/fish/functions/fdo.fish
@@ -1,9 +1,19 @@
 
-function fdo --description "Fuzzy find & vim"
+function fdo --description "Fuzzy find & open"
+
+  # list of extensions that should be opened with preferred application
+  set ext_exceptions 'pdf'
+
   fzf | read -l result
 
   if [ -n "$result" ]
-    command $EDITOR $result
+    set ext (string split -r -m1 '.' $result)
+
+    if not contains $ext[2] $ext_exceptions
+      command $EDITOR $result
+    else
+      open $result
+    end
   end
 
   commandline -f repaint
