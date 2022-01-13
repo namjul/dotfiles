9b0e4c7c9615c8b1929928eb653e837f7b3ce7f5
Samuel Hobl <nam@hobl.at>
2022-01-13 08:10:20 +0100

    feat(nvim): configure bullets.vim

--- dots/.config/nvim/after/plugin/bullets.rc.lua
+++ dots/.config/nvim/after/plugin/bullets.rc.lua
@@ -1,3 +1,7 @@
 local util = require('namjul.utils')
 
-util.var.g({ bullets_checkbox_markers = ' .oOX' })
+util.var.g({
+  bullets_checkbox_markers = ' ...x',
+  bullets_outline_levels = { 'num', 'std-' },
+  bullets_pad_right = 0,
+})
