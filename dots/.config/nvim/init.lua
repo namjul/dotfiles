c38e404776df0feb4fadcf2e7a8e0d995c0b5ad6
Samuel Hobl <nam@hobl.at>
2022-08-04 15:27:51 +0200

    chore(nvim): only load dendron neovim plugin in non vscode env

--- dots/.config/nvim/init.lua
+++ dots/.config/nvim/init.lua
@@ -232,10 +232,12 @@ util.createAugroup({
 
 require('namjul.mappings')
 
-require('neuron').setup({
-  virtual_titles = true,
-  mappings = true,
-  run = nil, -- function to run when in neuron dir
-  neuron_dir = '~/code/neuron/doc', -- the directory of all of your notes, expanded by default (currently supports only one directory for notes, find a way to detect neuron.dhall to use any directory)
-  leader = 'gz', -- the leader key to for all mappings, remember with 'go zettel'
-})
+if not util.isVsCode() then
+  require('neuron').setup({
+    virtual_titles = true,
+    mappings = true,
+    run = nil, -- function to run when in neuron dir
+    neuron_dir = '~/code/neuron/doc', -- the directory of all of your notes, expanded by default (currently supports only one directory for notes, find a way to detect neuron.dhall to use any directory)
+    leader = 'gz', -- the leader key to for all mappings, remember with 'go zettel'
+  })
+end
