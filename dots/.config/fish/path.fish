0988b04c302ce43e40a6b16156ce1e862424591c
Samuel Hobl <nam@hobl.at>
2022-08-10 00:10:30 +0200

    fix(path): typo

--- dots/.config/fish/path.fish
+++ dots/.config/fish/path.fish
@@ -2,4 +2,4 @@ test -d ~/.dotfiles/bin && set PATH $PATH ~/.dotfiles/bin
 test -d ~/bin && set PATH $PATH ~/bin
 test -d ~/Dropbox/bin && set PATH $PATH ~/Dropbox/bin
 test -d ~/.local/bin && set PATH $PATH ~/.local/bin
-test -d ~/.cargo/bin && set PATH $PATH ~/.cargeo/bin
+test -d ~/.cargo/bin && set PATH $PATH ~/.cargo/bin
