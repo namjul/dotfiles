a1a51420ac053388e7b2ed1bfe47c74894ebedb9
Samuel Hobl <samuel.hobl@gmail.com>
2023-02-16 18:34:43 +0100

    feat(nvim): add luasnips

--- dots/.config/nvim/after/plugin/luasnip.lua
+++ dots/.config/nvim/after/plugin/luasnip.lua
@@ -4,48 +4,48 @@ if not has_luasnip then
   return
 end
 
-require('luasnip')
+local ls = require('luasnip')
 
 
 -- https://github.com/dendronhq/dendron-template/blob/master/vault/.vscode/dendron.code-snippets
 
+local getUrlTitle = require('namjul.functions.getUrlTitle')
+local s = ls.snippet
+local sn = ls.snippet_node
+local t = ls.text_node
+local i = ls.insert_node
+local f = ls.function_node
+-- local c = ls.choice_node
+local d = ls.dynamic_node
+-- local r = ls.restore_node
+-- local types = require('luasnip.util.types')
+
 -- already exists in friendly-snippets with `task`
--- ls.add_snippets('markdown', {
---   s('td', {
---     t('- [ ] '),
---     i(1)
---   })
--- }, { key = 'all' })
---
--- local getUrlTitle = require('namjul.functions.getUrlTitle')
--- local s = ls.snippet
--- local sn = ls.snippet_node
--- local t = ls.text_node
--- local i = ls.insert_node
--- local f = ls.function_node
--- -- local c = ls.choice_node
--- local d = ls.dynamic_node
--- -- local r = ls.restore_node
--- -- local types = require('luasnip.util.types')
---
--- ls.add_snippets('markdown', {
---   s('[c', {
---     t('['),
---     d(1, function()
---       local copy = string.gsub(vim.fn.getreg('+'), '\n', '')
---       local title = getUrlTitle(copy)
---       return sn(nil, {
---         i(1, title or copy),
---       })
---     end, { 1 }),
---
---     t(']('),
---     f(function()
---       return string.gsub(vim.fn.getreg('+'), '\n', '')
---     end, {}),
---     t(')'),
---     i(0),
---   }),
--- }, { key = 'all' })
+ls.add_snippets('markdown', {
+  s('td', {
+    t('- [ ] '),
+    i(1)
+  })
+}, { key = 'all' })
+
+ls.add_snippets('markdown', {
+  s('[c', {
+    t('['),
+    d(1, function()
+      local copy = string.gsub(vim.fn.getreg('+'), '\n', '')
+      local title = getUrlTitle(copy)
+      return sn(nil, {
+        i(1, title or copy),
+      })
+    end, { 1 }),
+
+    t(']('),
+    f(function()
+      return string.gsub(vim.fn.getreg('+'), '\n', '')
+    end, {}),
+    t(')'),
+    i(0),
+  }),
+}, { key = 'all' })
 
 require('luasnip.loaders.from_vscode').lazy_load()
