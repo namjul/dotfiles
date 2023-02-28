25e3784f7657c9466286264688cb388de9abaafb
Samuel Hobl <samuel.hobl@gmail.com>
2023-02-28 10:11:21 +0100

    fix(set-colorscheme): make switch to work in neovim
    removing echos seems to make it work again

--- bin/nvim-ctrl.sh
+++ bin/nvim-ctrl.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # send command to all nvim instances
+# run `nvim-ctrl.sh ':set background=dark<CR>'`
 
 ls $XDG_RUNTIME_DIR/nvim.*.0 | xargs -I {} nvim --server {} --remote-send "$1"
 
