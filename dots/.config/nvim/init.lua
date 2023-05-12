de5d209e83e68b2332377c43acf876b3294c81cd
Samuel Hobl <samuel.hobl@gmail.com>
2023-05-12 08:48:51 +0200

    feat(nvim): adjust listchars signs

--- dots/.config/nvim/init.lua
+++ dots/.config/nvim/init.lua
@@ -174,7 +174,13 @@ opt.w({
   cursorline = true, -- Highlight current line
   wrap = false, -- don't wrap lines
   list = true, -- show whitespaces
-  listchars = 'tab:>\\ ,trail:-,extends:>,precedes:<,nbsp:+',
+  listchars = table.concat({
+    'nbsp:⦸', -- CIRCLED REVERSE SOLIDUS (U+29B8, UTF-8: E2 A6 B8)
+    'extends:»', -- RIGHT-POINTING DOUBLE ANGLE QUOTATION MARK (U+00BB, UTF-8: C2 BB)
+    'precedes:«', -- LEFT-POINTING DOUBLE ANGLE QUOTATION MARK (U+00AB, UTF-8: C2 AB)
+    'tab:▷⋯', -- WHITE RIGHT-POINTING TRIANGLE (U+25B7, UTF-8: E2 96 B7) + MIDLINE HORIZONTAL ELLIPSIS (U+22EF, UTF-8: E2 8B AF)
+    'trail:•', -- BULLET (U+2022, UTF-8: E2 80 A2)
+  }, ','),
 })
 
 -- Buffer
