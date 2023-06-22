d78d5c883996a977b2518a8ec3443181f6023d90
Samuel Hobl <samuel.hobl@gmail.com>
2023-06-22 10:34:31 +0200

    fix(nvim): add fidget setup call

--- dots/.config/nvim/init.lua
+++ dots/.config/nvim/init.lua
@@ -283,6 +283,7 @@ require('clipboard-image').setup({
 })
 
 require('overseer').setup()
+require('fidget').setup()
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
