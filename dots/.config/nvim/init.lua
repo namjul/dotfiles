75cfbe7b1b67d5162e8b4105f378597ede8d4655
Samuel Hobl <samuel.hobl@gmail.com>
2023-08-19 13:40:00 +0200

    chore: format

--- dots/.config/nvim/init.lua
+++ dots/.config/nvim/init.lua
@@ -42,46 +42,46 @@ vim.g.markdown_composer_open_browser = 0
 
 local is_bootstrap = require('namjul.bootstrap').bootstrap_paq({
   'savq/paq-nvim',
-  { 'tpope/vim-sensible' }, -- sensible defaults
-  { 'tpope/vim-repeat' }, -- enables the repeat command to work with external plugins
-  { 'tpope/vim-fugitive' }, -- git integration
-  { 'rbong/vim-flog' }, -- git branch viewer
-  { 'tpope/vim-rhubarb' }, -- open files on github
-  { 'tpope/vim-unimpaired' }, -- set of complementary pair commands
-  { 'tpope/vim-abolish' }, -- Case-preserving find and replace
-  { 'kylechui/nvim-surround' }, -- adds operators for surrounding characters
-  { 'numToStr/Comment.nvim' }, -- "gc" to comment visual regions/lines
-  { 'svermeulen/vim-cutlass' }, -- seperate `cut` form `delete`
+  { 'tpope/vim-sensible' },        -- sensible defaults
+  { 'tpope/vim-repeat' },          -- enables the repeat command to work with external plugins
+  { 'tpope/vim-fugitive' },        -- git integration
+  { 'rbong/vim-flog' },            -- git branch viewer
+  { 'tpope/vim-rhubarb' },         -- open files on github
+  { 'tpope/vim-unimpaired' },      -- set of complementary pair commands
+  { 'tpope/vim-abolish' },         -- Case-preserving find and replace
+  { 'kylechui/nvim-surround' },    -- adds operators for surrounding characters
+  { 'numToStr/Comment.nvim' },     -- "gc" to comment visual regions/lines
+  { 'svermeulen/vim-cutlass' },    -- seperate `cut` form `delete`
   { 'svermeulen/vim-subversive' }, -- adds a subsitute operator
   { 'nvim-lualine/lualine.nvim' }, -- fancier statusline
-  { 'gbprod/yanky.nvim' }, -- adds easy access to history of yanks
-  { 'tpope/vim-sleuth' }, -- support editor config files (https://editorconfig.org/)
+  { 'gbprod/yanky.nvim' },         -- adds easy access to history of yanks
+  { 'tpope/vim-sleuth' },          -- support editor config files (https://editorconfig.org/)
   { 'nvim-lua/plenary.nvim' },
-  { 'nvim-telescope/telescope.nvim', branch = '0.1.x' },
-  { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' },
+  { 'nvim-telescope/telescope.nvim',               branch = '0.1.x' },
+  { 'nvim-telescope/telescope-fzf-native.nvim',    run = 'make' },
   { 'nvim-telescope/telescope-live-grep-args.nvim' },
   { 'ThePrimeagen/harpoon' }, -- navigation helper
   { 'ekickx/clipboard-image.nvim' },
   { 'euclio/vim-markdown-composer' },
-  { 'preservim/vimux' }, -- allows to send commands from vim to tmux
-  { 'tyewang/vimux-jest-test' }, -- simplifies running jest test from vim
-  { 'tpope/vim-vinegar' }, -- file explorer TODO try again replacing with elihunter173/dirbuf.nvim
+  { 'preservim/vimux' },                   -- allows to send commands from vim to tmux
+  { 'tyewang/vimux-jest-test' },           -- simplifies running jest test from vim
+  { 'tpope/vim-vinegar' },                 -- file explorer TODO try again replacing with elihunter173/dirbuf.nvim
   { 'jeffkreeftmeijer/vim-numbertoggle' }, -- improves the display of line numbers
-  { 'windwp/nvim-autopairs' }, -- auto closes pairs
-  { 'Valloric/MatchTagAlways' }, -- highlights xml tags enclosing the cursor
-  { 'simeji/winresizer' }, -- helper for resizing windows
+  { 'windwp/nvim-autopairs' },             -- auto closes pairs
+  { 'Valloric/MatchTagAlways' },           -- highlights xml tags enclosing the cursor
+  { 'simeji/winresizer' },                 -- helper for resizing windows
   { 'lewis6991/gitsigns.nvim' },
-  { 'rhysd/committia.vim' }, -- improves vim 'commit' buffer
-  { 'L3MON4D3/LuaSnip' }, -- snippets engine
-  { 'rafamadriz/friendly-snippets' }, -- general snippets collection
-  { 'mattn/webapi-vim' }, -- needed for `gist-vim`
-  { 'mattn/gist-vim' }, -- interact with github gist from vim
-  { 'uga-rosa/ccc.nvim' }, -- Super powerful color picker / colorizer plugin.
-  { 'dkarter/bullets.vim' }, -- enhance bullet points management TODO replace with 'gaoDean/autolist.nvim' when checkboxes are supported
-  { 'csexton/trailertrash.vim' }, -- highlight trailing whitespace
-  { 'godlygeek/tabular' }, -- auto alignment
-  { 'tpope/vim-obsession' }, -- helper to start vim sessions
-  { 'ellisonleao/glow.nvim' }, -- markdown preview
+  { 'rhysd/committia.vim' },               -- improves vim 'commit' buffer
+  { 'L3MON4D3/LuaSnip' },                  -- snippets engine
+  { 'rafamadriz/friendly-snippets' },      -- general snippets collection
+  { 'mattn/webapi-vim' },                  -- needed for `gist-vim`
+  { 'mattn/gist-vim' },                    -- interact with github gist from vim
+  { 'uga-rosa/ccc.nvim' },                 -- Super powerful color picker / colorizer plugin.
+  { 'dkarter/bullets.vim' },               -- enhance bullet points management TODO replace with 'gaoDean/autolist.nvim' when checkboxes are supported
+  { 'csexton/trailertrash.vim' },          -- highlight trailing whitespace
+  { 'godlygeek/tabular' },                 -- auto alignment
+  { 'tpope/vim-obsession' },               -- helper to start vim sessions
+  { 'ellisonleao/glow.nvim' },             -- markdown preview
   {
     'nvim-treesitter/nvim-treesitter',
     run = function()
@@ -100,24 +100,31 @@ local is_bootstrap = require('namjul.bootstrap').bootstrap_paq({
   { 'williamboman/mason-lspconfig.nvim' },
   { 'neovim/nvim-lspconfig' },
   { 'jose-elias-alvarez/null-ls.nvim' },
-  { 'j-hui/fidget.nvim', branch = 'legacy' }, -- Standalone UI for nvim-lsp progress
+  { 'j-hui/fidget.nvim',                          branch = 'legacy' }, -- Standalone UI for nvim-lsp progress
   { 'folke/neodev.nvim' },
-  { 'AndrewRadev/switch.vim' }, -- fast boolean switch
+  { 'AndrewRadev/switch.vim' },               -- fast boolean switch
   { 'hrsh7th/nvim-cmp' },
   { 'hrsh7th/cmp-nvim-lsp' },
   { 'hrsh7th/cmp-path' },
   { 'hrsh7th/cmp-buffer' },
   { 'saadparwaiz1/cmp_luasnip' }, -- nvim-cmp source for Luasnip
   { 'folke/which-key.nvim' },
-  { 'abecodes/tabout.nvim' }, -- tabbing out from parentheses, quotes, and similar contexts today.
-  { 'rest-nvim/rest.nvim' }, -- http client in neovim
-  { 'monaqa/dial.nvim' }, -- enhanced increment/decrement plugin for Neovim.
-  { 'wsdjeg/vim-fetch' }, -- enables to process line and column jump specifications
+  { 'abecodes/tabout.nvim' },     -- tabbing out from parentheses, quotes, and similar contexts today.
+  { 'rest-nvim/rest.nvim' },      -- http client in neovim
+  { 'monaqa/dial.nvim' },         -- enhanced increment/decrement plugin for Neovim.
+  { 'wsdjeg/vim-fetch' },         -- enables to process line and column jump specifications
   { 'andrewferrier/debugprint.nvim' },
   { 'michaelb/sniprun' },
   { 'ggandor/leap.nvim' },
   { 'stevearc/overseer.nvim' },
   { 'stevearc/dressing.nvim' },
+  { 'MunifTanjim/nui.nvim' }, -- required by chatgpt.nvim
+  {
+    'jackmort/chatgpt.nvim',
+    run = function()
+      require('chatgpt').setup()
+    end,
+  },
 })
 
 if is_bootstrap then
@@ -130,14 +137,14 @@ end
 
 -- Global
 opt.g({
-  mouse = 'a', -- Enable Mouse clicking
+  mouse = 'a',        -- Enable Mouse clicking
   shortmess = table.concat({
-    't', -- truncate file message if too long to prevent 'Press Enter' message
-    'A', -- ignore annoying swapfile messages
-    'I', -- Don’t show the intro message when starting Vim
-    'O', -- file-read message overwrites previous
+    't',              -- truncate file message if too long to prevent 'Press Enter' message
+    'A',              -- ignore annoying swapfile messages
+    'I',              -- Don’t show the intro message when starting Vim
+    'O',              -- file-read message overwrites previous
   }, ''),
-  visualbell = true, --  Use visual bell instead of audible bell
+  visualbell = true,  --  Use visual bell instead of audible bell
   backupcopy = 'yes', -- optimize webpack watch option and also crontab editing
   clipboard = 'unnamedplus',
   ignorecase = true,
@@ -145,7 +152,7 @@ opt.g({
   wildignorecase = true,
   hidden = true,
   termguicolors = true, -- Enable term 24 bit colour
-  gdefault = true, -- Add the g flag to search/replace by default
+  gdefault = true,      -- Add the g flag to search/replace by default
   background = 'dark',
   pastetoggle = '<F2>',
   tabline = '%!v:lua.mytabline()',
@@ -153,7 +160,7 @@ opt.g({
   guifont = 'Cascadia_Code',
   completeopt = 'menu,menuone,noselect',
   showmode = false, -- hide status line at the bottom
-   tabstop = 2 -- spaces per tab
+  tabstop = 2,      -- spaces per tab
 })
 
 -- opt.g currenlty does not support tables
@@ -163,16 +170,16 @@ vim.opt.fillchars = {
 
 -- Window
 opt.w({
-  number = true, -- Show relative lines numbers
+  number = true,     -- Show relative lines numbers
   cursorline = true, -- Highlight current line
-  wrap = false, -- don't wrap lines
-  list = true, -- show whitespaces
+  wrap = false,      -- don't wrap lines
+  list = true,       -- show whitespaces
   listchars = table.concat({
-    'nbsp:⦸', -- CIRCLED REVERSE SOLIDUS (U+29B8, UTF-8: E2 A6 B8)
-    'extends:»', -- RIGHT-POINTING DOUBLE ANGLE QUOTATION MARK (U+00BB, UTF-8: C2 BB)
-    'precedes:«', -- LEFT-POINTING DOUBLE ANGLE QUOTATION MARK (U+00AB, UTF-8: C2 AB)
-    'tab:»·', -- WHITE RIGHT-POINTING TRIANGLE (U+25B7, UTF-8: E2 96 B7) + MIDLINE HORIZONTAL ELLIPSIS (U+22EF, UTF-8: E2 8B AF)
-    'trail:•', -- BULLET (U+2022, UTF-8: E2 80 A2)
+    'nbsp:⦸',      -- CIRCLED REVERSE SOLIDUS (U+29B8, UTF-8: E2 A6 B8)
+    'extends:»',    -- RIGHT-POINTING DOUBLE ANGLE QUOTATION MARK (U+00BB, UTF-8: C2 BB)
+    'precedes:«',   -- LEFT-POINTING DOUBLE ANGLE QUOTATION MARK (U+00AB, UTF-8: C2 AB)
+    'tab:»·',      -- WHITE RIGHT-POINTING TRIANGLE (U+25B7, UTF-8: E2 96 B7) + MIDLINE HORIZONTAL ELLIPSIS (U+22EF, UTF-8: E2 8B AF)
+    'trail:•',     -- BULLET (U+2022, UTF-8: E2 80 A2)
   }, ','),
 })
 
@@ -192,18 +199,18 @@ if util.readable(vim.fn.expand('~/.vimrc_background')) then
 end
 
 util.createAugroup({
-  { 'BufEnter', '*', 'lua', 'require"namjul.autocmds".bufEnter()' },
-  { 'BufLeave', '*', 'lua', 'require"namjul.autocmds".bufLeave()' },
-  { 'BufWinEnter', '*', 'lua', 'require"namjul.autocmds".bufWinEnter()' },
-  { 'BufWritePost', '*', 'lua', 'require"namjul.autocmds".bufWritePost()' },
-  { 'FocusGained', '*', 'lua', 'require"namjul.autocmds".focusGained()' },
-  { 'FocusLost', '*', 'lua', 'require"namjul.autocmds".focusLost()' },
-  { 'InsertEnter', '*', 'lua', 'require"namjul.autocmds".insertEnter()' },
-  { 'InsertLeave', '*', 'lua', 'require"namjul.autocmds".insertLeave()' },
-  { 'VimEnter', '*', 'lua', 'require"namjul.autocmds".vimEnter()' },
-  { 'WinEnter', '*', 'lua', 'require"namjul.autocmds".winEnter()' },
-  { 'WinLeave', '*', 'lua', 'require"namjul.autocmds".winLeave()' },
-  { 'InsertLeave', '*', 'set nopaste' }, --Disable paste mode on leaving insert mode.
+  { 'BufEnter',     '*', 'lua',        'require"namjul.autocmds".bufEnter()' },
+  { 'BufLeave',     '*', 'lua',        'require"namjul.autocmds".bufLeave()' },
+  { 'BufWinEnter',  '*', 'lua',        'require"namjul.autocmds".bufWinEnter()' },
+  { 'BufWritePost', '*', 'lua',        'require"namjul.autocmds".bufWritePost()' },
+  { 'FocusGained',  '*', 'lua',        'require"namjul.autocmds".focusGained()' },
+  { 'FocusLost',    '*', 'lua',        'require"namjul.autocmds".focusLost()' },
+  { 'InsertEnter',  '*', 'lua',        'require"namjul.autocmds".insertEnter()' },
+  { 'InsertLeave',  '*', 'lua',        'require"namjul.autocmds".insertLeave()' },
+  { 'VimEnter',     '*', 'lua',        'require"namjul.autocmds".vimEnter()' },
+  { 'WinEnter',     '*', 'lua',        'require"namjul.autocmds".winEnter()' },
+  { 'WinLeave',     '*', 'lua',        'require"namjul.autocmds".winLeave()' },
+  { 'InsertLeave',  '*', 'set nopaste' }, --Disable paste mode on leaving insert mode.
 }, 'namjulautocmds')
 
 util.createAugroup({
@@ -211,7 +218,7 @@ util.createAugroup({
 }, 'namjulskeletons')
 
 util.createAugroup({
-  { 'BufRead', 'tsconfig*.json', 'set filetype=jsonc' },
+  { 'BufRead',    'tsconfig*.json', 'set filetype=jsonc' },
   { 'BufNewFile', 'tsconfig*.json', 'set filetype=jsonc' },
 }, 'JsoncFilterType')
 
@@ -221,11 +228,10 @@ require('dressing').setup({
   },
 })
 
-
 vim.filetype.add({
   extension = {
-    mdx = 'mdx'
-  }
+    mdx = 'mdx',
+  },
 })
 
 require('namjul.translator')
