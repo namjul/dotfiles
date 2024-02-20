dc8823bc1b45288bd5450fd8abd9154a8b25dd8d
Samuel Hobl <samuel.hobl@gmail.com>
2024-02-20 12:51:48 +0100

    feat(wezterm): disable default `ALT-Enter` keybinding

--- dots/.config/wezterm/wezterm.lua
+++ dots/.config/wezterm/wezterm.lua
@@ -5,4 +5,13 @@ config.color_scheme = 'Gruvbox Dark (Gogh)'
 config.font = wezterm.font 'JetBrains Mono'
 config.enable_tab_bar = false
 
+config.keys = {
+  -- Turn off the default ALT-Enter Fullscreen action, since i3 already does this and frees the binding.
+  {
+    key = 'Enter',
+    mods = 'ALT',
+    action = wezterm.action.DisableDefaultAssignment,
+  },
+}
+
 return config
