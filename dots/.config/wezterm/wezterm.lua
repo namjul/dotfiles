b33dede5101a5dd0fcb73398e462dc92ef057ef6
Samuel Hobl <samuel.hobl@gmail.com>
2024-03-28 11:34:39 +0100

    fix(wezterm): repair colorscheme switch

--- dots/.config/wezterm/wezterm.lua
+++ dots/.config/wezterm/wezterm.lua
@@ -1,8 +1,16 @@
-local wezterm = require 'wezterm'
-local config = {}
+local wezterm = require('wezterm')
 
-config.color_scheme = 'Gruvbox Dark (Gogh)'
-config.font = wezterm.font 'JetBrains Mono'
+-- Allow working with both the current release and the nightly
+local config = {}
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+local light = 'GruvboxLight'
+local dark = 'GruvboxDark'
+
+config.color_scheme = dark
+config.font = wezterm.font('JetBrains Mono')
 config.enable_tab_bar = false
 
 config.keys = {
@@ -13,5 +21,23 @@ config.keys = {
     action = wezterm.action.DisableDefaultAssignment,
   },
 }
+
+-- listen to user vars and change configs
+-- https://wezfurlong.org/wezterm/recipes/passing-data.html#user-vars
+wezterm.on('user-var-changed', function(window, pane, name, value)
+  local overrides = window:get_config_overrides() or {}
+
+  -- change color_theme
+  if name == 'theme' then
+    if value == 'dark' then
+      overrides.color_scheme = dark
+    end
+    if value == 'light' then
+      overrides.color_scheme = light
+    end
+  end
+
+  window:set_config_overrides(overrides)
+end)
 
 return config
