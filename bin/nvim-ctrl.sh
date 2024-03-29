39a6b2920bd3956a149dc5f0fc57753296e476bb
Samuel Hobl <samuel.hobl@gmail.com>
2024-03-29 08:39:09 +0100

    fix(bin/set-colorscheme): supressing `nvim-ctrl` error

--- bin/nvim-ctrl.sh
+++ bin/nvim-ctrl.sh
@@ -3,5 +3,5 @@
 # send command to all nvim instances
 # run `nvim-ctrl.sh ':set background=dark<CR>'`
 
-ls $XDG_RUNTIME_DIR/nvim.*.0 | xargs -I {} nvim --server {} --remote-send "$1"
+ls $XDG_RUNTIME_DIR/nvim.*.0 2>/dev/null | xargs -I {} nvim --server {} --remote-send "$1"
 
