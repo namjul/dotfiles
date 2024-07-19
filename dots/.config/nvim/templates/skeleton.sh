b6d755dd0c47ec832b8e9486d6d6292d8ea799e9
Samuel Hobl <samuel.hobl@gmail.com>
2024-07-19 10:45:18 +0200

    feat(nvim): adjust sh skeleton

--- dots/.config/nvim/templates/skeleton.sh
+++ dots/.config/nvim/templates/skeleton.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-set -euxo pipefail # https://github.com/guettli/programming-guidelines#shell-scripts-are-ok-if-
+# https://github.com/guettli/programming-guidelines?tab=readme-ov-file#shell-scripts-are-ok-if-
+trap 'echo "Warning: A command has failed. Exiting the script. Line was ($0:$LINENO): $(sed -n "${LINENO}p" "$0")"; exit 3' ERR
+set -Eeuo pipefail
