074de6b89c84b8d1eb09e5a7a3838d295ffa9732
Samuel Hobl <samuel.hobl@gmail.com>
2024-07-24 09:21:49 +0200

    feat(path): change Dropbox bin location

--- dots/.config/fish/path.fish
+++ dots/.config/fish/path.fish
@@ -1,7 +1,7 @@
 fish_add_path ~/.dotfiles/bin
 fish_add_path ~/.dotfiles/bin/wm
 fish_add_path ~/.bin
-fish_add_path ~/Dropbox/.bin
+fish_add_path ~/Dropbox/opt/bin
 fish_add_path ~/.local/bin
 fish_add_path ~/.cargo/bin
 fish_add_path ~/go/bin
