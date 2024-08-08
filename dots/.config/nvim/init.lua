1d44aa4a1aa005c88614e647743564e5ad9feb23
Samuel Hobl <samuel.hobl@gmail.com>
2024-08-08 15:45:55 +0200

    feat(nvim): changes oil settings

--- dots/.config/nvim/init.lua
+++ dots/.config/nvim/init.lua
@@ -311,6 +311,10 @@ require('oil').setup({
     ['<C-v>'] = 'actions.select_vsplit',
     ['<C-s>'] = 'actions.select_split',
   },
+  skip_confirm_for_simple_edits = true,
+  view_options = {
+    show_hidden = true
+  }
 })
 require('hlargs').setup()
 require('conform').setup({
