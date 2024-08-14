7199d64caa9e039a00d8fb1cd47db92dd12f86df
Samuel Hobl <samuel.hobl@gmail.com>
2024-08-14 08:56:06 +0200

    feat(alias): new docker abbr + gestalt refactoring

--- dots/.config/fish/alias.fish
+++ dots/.config/fish/alias.fish
@@ -3,12 +3,6 @@
 # 02. ALIASES                                                                #
 ##############################################################################
 
-# quick switches to folders
-abbr dd 'cd $HOME/.dotfiles'
-abbr db 'cd ~/Dropbox'
-abbr dc 'cd ~/code'
-abbr dls 'cd ~/.local/share'
-
 # quick edits
 alias e $EDITOR
 alias ev 'nvim ~/.dotfiles/dots/.config/nvim/init.lua'
@@ -16,46 +10,70 @@ alias ed 'nvim ~/.dotfiles/dots/'
 alias ef 'nvim ~/.dotfiles/dots/.config/fish/config.fish'
 alias et 'nvim ~/Dropbox/todo'
 
+# navigation
 abbr d cd
 abbr .. 'cd ..'
 abbr ... 'cd ../..'
 abbr .... 'cd ../../..'
 abbr ..... 'cd ../../../..'
 abbr --add -- - 'cd -'
+# quick switches to folders
+abbr dd 'cd $HOME/.dotfiles'
+abbr db 'cd ~/Dropbox'
+abbr dc 'cd ~/code'
+abbr dls 'cd ~/.local/share'
 
+# shell
 abbr h 'history'
 abbr j 'jobs'
 abbr l 'less -R'
+abbr x exit
 
+# git
+abbr g 'git'
+abbr grn 'git rebase -i HEAD~' # git interactive rebase to n
+
+# translation
+abbr en 'trans :en'
+abbr de 'trans :de'
+
+# node
+abbr repl 'NODE_PATH=(npm root -g) node'
+abbr run 'dum run' # shorthand for npm run
+abbr nv 'node --version'
+
+# timewarrior
+abbr tw timew
+abbr tws timew start
+abbr twsum timew summary :annotation :ids :week
+
+# todo
+abbr t todo
+abbr ts 'toggl start'
+abbr tp 'toggl stop'
+abbr tc 'toggl continue'
+abbr tn 'toggl now'
+abbr tsm 'toggl sum'
+
+# tmux
+abbr tb "tmux new -s (pwd | sed 's/.*\///g')" # begin tmux session
+
+# docker
+abbr dcu "docker compose up"
+abbr dsa "docker stop (docker ps -a -q)"
+
+# misc
 abbr mv 'mv -i'
 abbr cp 'cp -i -p'
 abbr grep 'grep --color=auto'
-abbr g 'git'
 abbr week 'date +%V' # Get week number
 abbr path 'echo $PATH | tr -s " " "\n"' # Pretty print the path
 alias cat="bat" # Drop-in replacement for cat TODO add  --theme=gruvbox-(set-colorscheme) but a fast implementation
 abbr tmp ' cd (mktemp -d)'
 abbr pbcopy 'xclip -selection clipboard' # replicate pbcopy from macos
 abbr pbpaste 'xclip -selection clipboard -o'
-abbr x exit
-abbr t todo
-abbr tw timew
-abbr tws timew start
-abbr twsum timew summary :annotation :ids :week
-abbr en 'trans :en'
-abbr de 'trans :de'
-abbr grn 'git rebase -i HEAD~' # git interactive rebase to n
-abbr run 'dum run' # shorthand for npm run
-abbr nv 'node --version'
 abbr untar 'tar -xvf' # extract .tar.gz
 abbr --add unset 'set --erase' # remove env variable
-abbr repl 'NODE_PATH=(npm root -g) node'
-abbr ts 'toggl start'
-abbr tp 'toggl stop'
-abbr tc 'toggl continue'
-abbr tn 'toggl now'
-abbr tsm 'toggl sum'
-abbr tb "tmux new -s (pwd | sed 's/.*\///g')" # begin tmux session
 
 # ls
 if type -q eza
