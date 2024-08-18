9715549be5410293c33ad2b26d5655d833add4ce
Samuel Hobl <samuel.hobl@gmail.com>
2024-08-18 10:07:37 +0200

    feat(systemd): create aspect + add ssh-agent

--- dots/.config/fish/config.fish
+++ dots/.config/fish/config.fish
@@ -29,9 +29,6 @@ if not type -q shellfirm
   echo "`shellfirm` binary is missing. see installation guide: https://github.com/kaplanelad/shellfirm"
 end
 
-# https://gist.github.com/dmccombs/6345e5b8319e3a060b64181c1784f567
-# set (gnome-keyring-daemon --start | string split "=")
-
 if status is-interactive
   set fish_greeting # remove fish's greeting
 
