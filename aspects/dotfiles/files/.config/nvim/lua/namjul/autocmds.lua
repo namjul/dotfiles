7f187ec9e89905783e5689b90909460833498ba1
Samuel Hobl <samuel.hobl@gmail.com>
2024-11-21 20:52:46 +0100

    feat(nvim): remove buflisted condition from BlurFocus support check

--- aspects/dotfiles/files/.config/nvim/lua/namjul/autocmds.lua
+++ aspects/dotfiles/files/.config/nvim/lua/namjul/autocmds.lua
@@ -20,9 +20,9 @@ end
 
 local function supportsBlurFocus(callback)
   local filetype = util.opt.b('filetype')
-  local listed = util.opt.b('buflisted')
+  -- local listed = util.opt.b('buflisted')
   local floatingWindow = vim.api.nvim_win_get_config(vim.api.nvim_get_current_win()).relative ~= ''
-  if autocmds.filetypeBlacklist[filetype] ~= true and listed and not floatingWindow or autocmds.filetypeWhitelist[filetype] == true then
+  if autocmds.filetypeBlacklist[filetype] ~= true and not floatingWindow or autocmds.filetypeWhitelist[filetype] == true then
     callback(filetype)
   end
 end
