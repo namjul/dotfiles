a6bac49077855fd6cc97c10e4f0143f369b77108
Samuel Hobl <samuel.hobl@gmail.com>
2024-11-21 21:37:47 +0100

    feat(nvim): add option for setting colorcolumn

--- aspects/dotfiles/files/.config/nvim/lua/namjul/autocmds.lua
+++ aspects/dotfiles/files/.config/nvim/lua/namjul/autocmds.lua
@@ -3,6 +3,35 @@ local cmd = vim.cmd
 
 local autocmds = {}
 
+-- stylua: ignore
+local focused_colorcolumn = '+' .. table.concat({
+  '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12',
+  '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23',
+  '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34',
+  '35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45',
+  '46', '47', '48', '49', '50', '51', '52', '53', '54', '55', '56',
+  '57', '58', '59', '60', '61', '62', '63', '64', '65', '66', '67',
+  '68', '69', '70', '71', '72', '73', '74', '75', '76', '77', '78',
+  '79', '80', '81', '82', '83', '84', '85', '86', '87', '88', '89',
+  '90', '91', '92', '93', '94', '95', '96', '97', '98', '99', '100',
+  '101', '102', '103', '104', '105', '106', '107', '108', '109', '110',
+  '111', '112', '113', '114', '115', '116', '117', '118', '119', '120',
+  '121', '122', '123', '124', '125', '126', '127', '128', '129', '130',
+  '131', '132', '133', '134', '135', '136', '137', '138', '139', '140',
+  '141', '142', '143', '144', '145', '146', '147', '148', '149', '150',
+  '151', '152', '153', '154', '155', '156', '157', '158', '159', '160',
+  '161', '162', '163', '164', '165', '166', '167', '168', '169', '170',
+  '171', '172', '173', '174', '175', '176', '177', '178', '179', '180',
+  '181', '182', '183', '184', '185', '186', '187', '188', '189', '190',
+  '191', '192', '193', '194', '195', '196', '197', '198', '199', '200',
+  '201', '202', '203', '204', '205', '206', '207', '208', '209', '210',
+  '211', '212', '213', '214', '215', '216', '217', '218', '219', '220',
+  '221', '222', '223', '224', '225', '226', '227', '228', '229', '230',
+  '231', '232', '233', '234', '235', '236', '237', '238', '239', '240',
+  '241', '242', '243', '244', '245', '246', '247', '248', '249', '250',
+  '251', '252', '253', '254'
+}, ',+')
+
 local winhighlight_blurred = table.concat({
   'CursorLineNr:LineNr',
   'EndOfBuffer:ColorColumn',
@@ -15,7 +44,7 @@ local winhighlight_blurred = table.concat({
 }, ',')
 
 local function set_cursorline(active)
-  util.opt.w({ cursorline = active })
+  vim.wo.cursorline = active
 end
 
 local function focus_window()
@@ -31,6 +60,10 @@ local function focus_window()
     vim.wo.winhighlight = ''
   end
 
+  if filetype == '' or autocmds.colorcolumn_filetype_blacklist[filetype] ~= true then
+    vim.wo.colorcolumn = focused_colorcolumn
+  end
+
   if filetype == '' then
     vim.wo.list = true
   else
@@ -44,11 +77,9 @@ local function focus_window()
 
   local conceallevel = autocmds.conceallevel_filetypes[filetype] or 2
   vim.wo.conceallevel = conceallevel
-
 end
 
 local function blur_window()
-
   local filetype = vim.bo.filetype
 
   -- Turn off relative numbers (and turn on non-relative numbers)
@@ -75,7 +106,6 @@ local function blur_window()
   if filetype == '' or autocmds.conceallevel_filetypes[filetype] == nil then
     vim.wo.conceallevel = 0
   end
-
 end
 
 -- local function rooter(ctx)
@@ -140,14 +170,14 @@ autocmds.winhighlight_filetype_blacklist = {
   ['fugitiveblame'] = true,
   ['undotree'] = true,
   ['qf'] = true,
-  ['TelescopePrompt'] = true
+  ['TelescopePrompt'] = true,
 }
 
 -- Force 'list' (when `true`) or 'nolist' (when `false`) for these.
 autocmds.list_filetypes = {
   ['help'] = false,
   ['shellbot'] = false,
-  ['TelescopePrompt'] = false
+  ['TelescopePrompt'] = false,
 }
 --
 -- Don't mess with 'conceallevel' for these.
@@ -164,7 +194,18 @@ autocmds.number_blacklist = {
   ['qf'] = true,
   ['shellbot'] = true,
   ['undotree'] = true,
-  ['TelescopePrompt'] = true
+  ['TelescopePrompt'] = true,
+}
+--
+-- Don't use colorcolumn when these filetypes get focus (we want them to appear
+-- full-width irrespective of 'textwidth').
+autocmds.colorcolumn_filetype_blacklist = {
+  ['TelescopePrompt'] = true,
+  ['diff'] = true,
+  ['oil'] = true,
+  ['fugitiveblame'] = true,
+  ['undotree'] = true,
+  ['qf'] = true,
 }
 
 return autocmds
