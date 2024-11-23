029fa6b8217f52326a1888472ebe2ea491976a2d
Samuel Hobl <samuel.hobl@gmail.com>
2024-11-23 11:31:13 +0100

    fix(theme): make sure plaintext function only sets on buffer

--- aspects/dotfiles/files/.config/nvim/lua/namjul/functions/plaintext.lua
+++ aspects/dotfiles/files/.config/nvim/lua/namjul/functions/plaintext.lua
@@ -1,46 +1,33 @@
-local util = require('namjul.utils')
 
 local function plaintext()
-  local opt = util.opt
-  local map = util.map
   if vim.fn.has('conceal') == 1 then
-    opt.w({ concealcursor = 'nc' })
+    vim.opt_local.concealcursor = 'nc'
   end
 
-  opt.w({
-    list = false,
-    linebreak = true,
-    wrap = true,
-  })
+  vim.opt_local.list = false
+  vim.opt_local.textwidth = 0
+  vim.opt_local.wrap = true
+  vim.opt_local.wrapmargin = 0
 
-  opt.b({
-    textwidth = 0,
-    wrapmargin = 0,
-  })
 
-  map.b('n', 'j', 'gj')
-  map.b('n', 'k', 'gk')
-  map.b('n', '0', 'g0')
-  map.b('n', '^', 'g^')
-  map.b('n', '$', 'g$')
-  map.b('n', 'j', 'gj')
-  map.b('n', 'k', 'gk')
-  map.b('n', '0', 'g0')
-  map.b('n', '^', 'g^')
-  map.b('n', '$', 'g$')
+  vim.keymap.set('n', 'j', 'gj', { buffer = true })
+  vim.keymap.set('n', 'k', 'gk', { buffer = true })
 
+  -- Break undo sequences into chunks (after punctuation); see: `:h i_CTRL-G_u`
   -- Create undo 'snapshots' when being in inline editing.
   --
   -- From:
   -- - https://github.com/wincent/wincent/blob/44b112f26ec6435a9b78e64225eb0f9082999c1e/aspects/vim/files/.vim/autoload/wincent/functions.vim#L32
   -- - https://twitter.com/vimgifs/status/913390282242232320
   --
-  map.b('i', '!', '!<C-g>u')
-  map.b('i', ',', ',<C-g>u')
-  map.b('i', '.', '.<C-g>u')
-  map.b('i', ':', ':<C-g>u')
-  map.b('i', ';', ';<C-g>u')
-  map.b('i', '?', '?<C-g>u')
+
+  vim.keymap.set('i', '!', '!<C-g>u', { buffer = true })
+  vim.keymap.set('i', ',', ',<C-g>u', { buffer = true })
+  vim.keymap.set('i', '.', '.<C-g>u', { buffer = true })
+  vim.keymap.set('i', ':', ':<C-g>u', { buffer = true })
+  vim.keymap.set('i', ';', ';<C-g>u', { buffer = true })
+  vim.keymap.set('i', '?', '?<C-g>u', { buffer = true })
+
 end
 
 return plaintext
