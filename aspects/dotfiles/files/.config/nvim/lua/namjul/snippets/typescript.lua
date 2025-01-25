616b766b574aaec37efb4f547aa6b65a8bd9269b
Samuel Hobl <samuel.hobl@gmail.com>
2025-01-25 12:10:49 +0100

    fix(nvim): correct snippet import path

--- aspects/dotfiles/files/.config/nvim/lua/namjul/snippets/typescript.lua
+++ aspects/dotfiles/files/.config/nvim/lua/namjul/snippets/typescript.lua
@@ -1,4 +1,4 @@
 local luasnip = require('luasnip')
-local typescript = require('wincent.snippets.common.typescript')
+local typescript = require('namjul.snippets.common.typescript')
 
 luasnip.add_snippets('typescript', typescript)
