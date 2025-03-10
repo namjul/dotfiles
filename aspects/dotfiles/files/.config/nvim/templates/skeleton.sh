ffdfed1899e984860355b8e052f972ddf26365f1
Samuel Hobl <samuel.hobl@gmail.com>
2025-03-10 11:51:26 +0100

    feat(nvim): update shell-script template

--- aspects/dotfiles/files/.config/nvim/templates/skeleton.sh
+++ aspects/dotfiles/files/.config/nvim/templates/skeleton.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# https://github.com/guettli/programming-guidelines?tab=readme-ov-file#shell-scripts-are-ok-if-
+# https://github.com/guettli/bash-strict-mode
 trap 'echo "Warning: A command has failed. Exiting the script. Line was ($0:$LINENO): $(sed -n "${LINENO}p" "$0")"; exit 3' ERR
 set -Eeuo pipefail
