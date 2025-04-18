640794bcb7495ca796c4a0d8be8c2cc384c52868
Samuel Hobl <samuel.hobl@gmail.com>
2025-04-18 10:55:01 +0200

    feat(nvim/shellbot): prevent unintentionally loosing valuable state from shellbot
    from https://github.com/wincent/wincent/blob/8395db860995df710561c8fe0e01bfd90238dbb9/aspects/nvim/files/.config/nvim/after/plugin/shellbot.lua#L52

--- aspects/dotfiles/files/.config/nvim/after/plugin/shellbot.lua
+++ aspects/dotfiles/files/.config/nvim/after/plugin/shellbot.lua
@@ -1,3 +1,35 @@
 local has_shellbot = pcall(require, 'chatbot')
-if has_shellbot then
+
+if not has_shellbot then
+  return
 end
+
+-- Set up an autocmd to stop me from accidentally quitting vim when shellbot is
+-- the only thing running in it. I do this all the time, losing valuable state.
+vim.api.nvim_create_autocmd('QuitPre', {
+  pattern = '*',
+  callback = function()
+    local buftype = vim.bo.buftype
+    local filetype = vim.bo.filetype
+    local win_count = #vim.api.nvim_tabpage_list_wins(0)
+
+    if filetype == 'shellbot' and buftype == 'nofile' and win_count == 1 then
+      vim.notify(
+        '\n'
+        .. '┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓\n'
+        .. '┃ Use :q! if you really want to quit the last shellbot window ┃\n'
+        .. '┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛\n'
+        .. '\n',
+        vim.log.levels.WARN
+      )
+
+      -- Will make Neovim abort the quit with:
+      --
+      --    E37: No write since last change
+      --    E162: No write since last change for buffer "[No Name]"
+      --
+      vim.bo.buftype = ''
+    end
+  end,
+})
+
