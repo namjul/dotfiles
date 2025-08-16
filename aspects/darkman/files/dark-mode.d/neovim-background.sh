47a36ffdc1af7722251f65fc1dcad49484db2960
Samuel Hobl <samuel.hobl@gmail.com>
2025-08-16 13:11:01 +0200

    feat(darkman): use without portal

---
+++ aspects/darkman/files/dark-mode.d/neovim-background.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+nvim-ctrl.sh 'set background=dark'
+echo 'set background=dark' >~/.vimrc_background
---
+++ aspects/darkman/files/light-mode.d/neovim-background.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+nvim-ctrl.sh 'set background=light'
+echo 'set background=light' >~/.vimrc_background
