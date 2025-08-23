50963fc4101b11b31aeae2589928a4903c70d0ad
Samuel Hobl <samuel.hobl@gmail.com>
2025-08-23 22:59:38 +0200

    feat(nvim): run linter only when executable is available

--- aspects/dotfiles/files/.config/nvim/after/plugin/nvim-lint.lua
+++ aspects/dotfiles/files/.config/nvim/after/plugin/nvim-lint.lua
@@ -3,13 +3,18 @@ local has_lint, lint = pcall(require, 'lint')
 
 if has_lint then
 
-  lint.linters_by_ft = {
-    javascript = { "eslint" },
-    javascriptreact = { "eslint" },
-    typescript = { "eslint" },
-    typescriptreact = { "eslint" },
-    vue = { "eslint" },
+  local linters = {
+    eslint = {
+      "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx", "vue" 
+    }
   }
+
+  -- register each linter by filetype
+  for linter, filetypes in pairs(linters) do
+    for _, ft in ipairs(filetypes) do
+      lint.linters_by_ft[ft] = { linter }
+    end
+  end
 
   local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
   vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
@@ -19,7 +24,14 @@ if has_lint then
       -- avoid superfluous noise, notably within the handy LSP pop-ups that
       -- describe the hovered symbol using Markdown.
       if vim.bo.modifiable then
-        require("lint").try_lint()
+
+        -- check if linter is is available
+        for linter in pairs(linters) do
+          if vim.fn.executable(lint.linters[linter].cmd()) == 1 then
+            lint.try_lint(linter)
+          end
+        end
+
       end
     end,
   })
