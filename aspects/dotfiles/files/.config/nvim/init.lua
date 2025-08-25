6ad7ee865e58a9573932e895d7bb064fd46366c7
Samuel Hobl <samuel.hobl@gmail.com>
2025-08-25 22:12:54 +0200

    feat(nvim): explicitly set snippet keybindings

--- aspects/dotfiles/files/.config/nvim/init.lua
+++ aspects/dotfiles/files/.config/nvim/init.lua
@@ -387,8 +387,23 @@ require('nvim-ts-autotag').setup()
 require('blink.cmp').setup({
   snippets = { preset = 'luasnip' },
   keymap = {
-    preset = 'enter',
+    preset = 'none',
     ['<C-o>'] = { 'show', 'show_documentation', 'hide_documentation' },
+    ['<C-e>'] = { 'hide', 'fallback' },
+    ['<CR>'] = { 'accept', 'fallback' },
+
+    ['<Tab>'] = { 'snippet_forward', 'fallback' },
+    ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
+
+    ['<Up>'] = { 'select_prev', 'fallback' },
+    ['<Down>'] = { 'select_next', 'fallback' },
+    ['<C-p>'] = { 'select_prev', 'fallback_to_mappings' },
+    ['<C-n>'] = { 'select_next', 'fallback_to_mappings' },
+
+    ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
+    ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
+
+    ['<C-k>'] = { 'show_signature', 'hide_signature', 'fallback' },
   },
   sources = {
     providers = {
