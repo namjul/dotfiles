3648b12a3fe77b04339f57ec3eb369fb1fdcf4d0
Samuel Hobl <samuel.hobl@gmail.com>
2025-09-02 17:18:30 +0200

    chore(dotfiles/i3): remove unused autostart script

--- aspects/dotfiles/files/.config/i3/autostart.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-grep -rh Exec ~/.config/autostart | while read -r line ; do 
-   ${line:5} &
-done
-
-# grep -rh Exec /etc/xdg/autostart | while read -r line ; do 
-#    ${line:5} &
-# done
