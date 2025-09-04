1d49e6428492991d40609d63d0f19ba64d4748ed
Samuel Hobl <samuel.hobl@gmail.com>
2025-09-04 23:10:36 +0200

    feat(nvim): add snippets to ft `javascriptreact` and `typescriptreact`

--- aspects/dotfiles/files/.config/nvim/lua/namjul/snippets/javascript.lua
+++ aspects/dotfiles/files/.config/nvim/lua/namjul/snippets/javascript.lua
@@ -2,3 +2,4 @@ local luasnip = require('luasnip')
 local typescript = require('namjul.snippets.common.typescript')
 
 luasnip.add_snippets('javascript', typescript)
+luasnip.add_snippets('javascriptreact', typescript)
--- aspects/dotfiles/files/.config/nvim/lua/namjul/snippets/typescript.lua
+++ aspects/dotfiles/files/.config/nvim/lua/namjul/snippets/typescript.lua
@@ -2,3 +2,4 @@ local luasnip = require('luasnip')
 local typescript = require('namjul.snippets.common.typescript')
 
 luasnip.add_snippets('typescript', typescript)
+luasnip.add_snippets('typescriptreact', typescript)
