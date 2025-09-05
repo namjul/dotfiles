a6361d92a0e52c23c6c88db17d1a21c08651be2b
Samuel Hobl <samuel.hobl@gmail.com>
2025-09-05 08:57:46 +0200

    feat(dotfiles/nvim): add hipatterns patterns

--- aspects/dotfiles/files/.config/nvim/plugin/plugins.lua
+++ aspects/dotfiles/files/.config/nvim/plugin/plugins.lua
@@ -121,6 +121,9 @@ local hipatterns = require('mini.hipatterns')
 hipatterns.setup({
   highlighters = {
     fixme = { pattern = '[Xx][Xx][Xx]', group = 'MiniHipatternsFixme' },
+    hack  = { pattern = '%f[%w]()HACK()%f[%W]',  group = 'MiniHipatternsHack'  },
+    todo  = { pattern = '%f[%w]()TODO()%f[%W]',  group = 'MiniHipatternsTodo'  },
+    note  = { pattern = '%f[%w]()NOTE()%f[%W]',  group = 'MiniHipatternsNote'  },
     hex_color = hipatterns.gen_highlighter.hex_color(),
   },
 })
