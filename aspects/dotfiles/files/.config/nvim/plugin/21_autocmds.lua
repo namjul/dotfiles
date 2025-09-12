eab76bdf0047e62fc1df1c82b91bc1ea3e183349
Samuel Hobl <samuel.hobl@gmail.com>
2025-09-12 18:44:17 +0200

    refactor(nvim): use native autocmd

--- aspects/dotfiles/files/.config/nvim/plugin/21_autocmds.lua
+++ aspects/dotfiles/files/.config/nvim/plugin/21_autocmds.lua
@@ -1,27 +1,88 @@
-local util = require('namjul.utils')
+-- Create autocommand group for general autocommands
+vim.api.nvim_create_augroup('NamjulAutocmds', { clear = true })
+vim.api.nvim_create_autocmd('BufEnter', {
+  pattern = '*',
+  callback = function() require('namjul.autocmds').bufEnter() end,
+  group = 'NamjulAutocmds',
+})
+vim.api.nvim_create_autocmd('BufLeave', {
+  pattern = '*',
+  callback = function() require('namjul.autocmds').bufLeave() end,
+  group = 'NamjulAutocmds',
+})
+vim.api.nvim_create_autocmd('BufWinEnter', {
+  pattern = '*',
+  callback = function() require('namjul.autocmds').bufWinEnter() end,
+  group = 'NamjulAutocmds',
+})
+vim.api.nvim_create_autocmd('BufWritePost', {
+  pattern = '*',
+  callback = function() require('namjul.autocmds').bufWritePost() end,
+  group = 'NamjulAutocmds',
+})
+vim.api.nvim_create_autocmd('FocusGained', {
+  pattern = '*',
+  callback = function() require('namjul.autocmds').focusGained() end,
+  group = 'NamjulAutocmds',
+})
+vim.api.nvim_create_autocmd('FocusLost', {
+  pattern = '*',
+  callback = function() require('namjul.autocmds').focusLost() end,
+  group = 'NamjulAutocmds',
+})
+vim.api.nvim_create_autocmd('InsertEnter', {
+  pattern = '*',
+  callback = function() require('namjul.autocmds').insertEnter() end,
+  group = 'NamjulAutocmds',
+})
+vim.api.nvim_create_autocmd('InsertLeave', {
+  pattern = '*',
+  callback = function() require('namjul.autocmds').insertLeave() end,
+  group = 'NamjulAutocmds',
+})
+vim.api.nvim_create_autocmd('InsertLeave', {
+  pattern = '*',
+  command = 'set nopaste',
+  group = 'NamjulAutocmds',
+})
+vim.api.nvim_create_autocmd('VimEnter', {
+  pattern = '*',
+  callback = function() require('namjul.autocmds').vimEnter() end,
+  group = 'NamjulAutocmds',
+})
+vim.api.nvim_create_autocmd('WinEnter', {
+  pattern = '*',
+  callback = function() require('namjul.autocmds').winEnter() end,
+  group = 'NamjulAutocmds',
+})
+vim.api.nvim_create_autocmd('WinLeave', {
+  pattern = '*',
+  callback = function() require('namjul.autocmds').winLeave() end,
+  group = 'NamjulAutocmds',
+})
 
-util.createAugroup({
-  { 'BufEnter', '*', 'lua', 'require"namjul.autocmds".bufEnter()' },
-  { 'BufLeave', '*', 'lua', 'require"namjul.autocmds".bufLeave()' },
-  { 'BufWinEnter', '*', 'lua', 'require"namjul.autocmds".bufWinEnter()' },
-  { 'BufWritePost', '*', 'lua', 'require"namjul.autocmds".bufWritePost()' },
-  { 'FocusGained', '*', 'lua', 'require"namjul.autocmds".focusGained()' },
-  { 'FocusLost', '*', 'lua', 'require"namjul.autocmds".focusLost()' },
-  { 'InsertEnter', '*', 'lua', 'require"namjul.autocmds".insertEnter()' },
-  { 'InsertLeave', '*', 'lua', 'require"namjul.autocmds".insertLeave()' },
-  { 'InsertLeave', '*', 'set nopaste' }, --Disable paste mode on leaving insert mode.
-  { 'VimEnter', '*', 'lua', 'require"namjul.autocmds".vimEnter()' },
-  { 'WinEnter', '*', 'lua', 'require"namjul.autocmds".winEnter()' },
-  { 'WinLeave', '*', 'lua', 'require"namjul.autocmds".winLeave()' },
-}, 'namjulautocmds')
+-- Create autocommand group for skeleton templates
+vim.api.nvim_create_augroup('NamjulSkeletons', { clear = true })
+vim.api.nvim_create_autocmd('BufNewFile', {
+  pattern = '*.sh',
+  callback = function() require('namjul.autocmds').skeleton('~/.config/nvim/templates/skeleton.sh') end,
+  group = 'NamjulSkeletons',
+})
+vim.api.nvim_create_autocmd('BufNewFile', {
+  pattern = '*.html',
+  callback = function() require('namjul.autocmds').skeleton('~/.config/nvim/templates/skeleton.html') end,
+  group = 'NamjulSkeletons',
+})
+vim.api.nvim_create_autocmd('BufNewFile', {
+  pattern = '*.http',
+  callback = function() require('namjul.autocmds').skeleton('~/.config/nvim/templates/skeleton.http') end,
+  group = 'NamjulSkeletons',
+})
 
-util.createAugroup({
-  { 'BufNewFile', '*.sh', 'lua', 'require"namjul.autocmds".skeleton("~/.config/nvim/templates/skeleton.sh")' },
-  { 'BufNewFile', '*.html', 'lua', 'require"namjul.autocmds".skeleton("~/.config/nvim/templates/skeleton.html")' },
-  { 'BufNewFile', '*.http', 'lua', 'require"namjul.autocmds".skeleton("~/.config/nvim/templates/skeleton.http")' },
-}, 'namjulskeletons')
-
-util.createAugroup({
-  { 'BufRead', 'tsconfig*.json', 'set filetype=jsonc' },
-  { 'BufNewFile', 'tsconfig*.json', 'set filetype=jsonc' },
-}, 'JsoncFilterType')
+-- Create autocommand group for JSONC filetype
+vim.api.nvim_create_augroup('JsoncFiletype', { clear = true })
+vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
+  pattern = 'tsconfig*.json',
+  command = 'set filetype=jsonc',
+  group = 'JsoncFiletype',
+})
