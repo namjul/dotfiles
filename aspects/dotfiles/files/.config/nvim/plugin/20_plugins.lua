1388abd4763c17289e4b94cd764246be7f99725f
Samuel Hobl <samuel.hobl@gmail.com>
2025-09-12 20:44:20 +0200

    feat(nvim): add keymaps to minifiles

--- aspects/dotfiles/files/.config/nvim/plugin/20_plugins.lua
+++ aspects/dotfiles/files/.config/nvim/plugin/20_plugins.lua
@@ -478,6 +478,28 @@ later(function()
   local minifiles = require('mini.files')
   minifiles.setup({ windows = { preview = true } })
 
+  local set_cwd = function()
+    local path = (minifiles.get_fs_entry() or {}).path
+    if path == nil then return vim.notify('Cursor is not on valid entry') end
+    vim.fn.chdir(vim.fs.dirname(path))
+  end
+
+  -- Yank in register full path of entry under cursor
+  local yank_path = function()
+    local path = (minifiles.get_fs_entry() or {}).path
+    if path == nil then return vim.notify('Cursor is not on valid entry') end
+    vim.fn.setreg(vim.v.register, path)
+  end
+
+  vim.api.nvim_create_autocmd('User', {
+    pattern = 'MiniFilesBufferCreate',
+    callback = function(args)
+      local b = args.data.buf_id
+      vim.keymap.set('n', 'g~', set_cwd, { buffer = b, desc = 'Set cwd' })
+      vim.keymap.set('n', 'gy', yank_path, { buffer = b, desc = 'Yank path' })
+    end,
+  })
+
   local dotfiles_augroup = vim.api.nvim_create_augroup('dotfiles', {})
   vim.api.nvim_create_autocmd('User', {
     group = dotfiles_augroup,
