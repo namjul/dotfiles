53d90e7f1a94360f1a9b319c41da0880491fcb02
Samuel Hobl <samuel.hobl@gmail.com>
2025-09-16 01:07:54 +0200

    feat(nvim): add deno lsp

--- aspects/nvim/files/.config/nvim/lua/namjul/lsp.lua
+++ aspects/nvim/files/.config/nvim/lua/namjul/lsp.lua
@@ -1,3 +1,5 @@
+local has_lspconfig, lspconfig = pcall(require, 'lspconfig')
+
 local lsp = {}
 
 vim.api.nvim_create_autocmd('LspAttach', {
@@ -35,6 +37,13 @@ lsp.init = function()
 
   local servers = {
     rust_analyzer = {},
+    denols = {
+      root_dir = function(bufnr, on_dir)
+        local root_markers = { 'deno.json' }
+        local project_root = vim.fs.root(bufnr, root_markers)
+        if project_root then on_dir(project_root) end
+      end,
+    },
     lua_ls = {
       settings = {
         Lua = {
@@ -60,6 +69,11 @@ lsp.init = function()
           },
         },
       },
+      root_dir = function(bufnr, on_dir)
+        local root_markers = { 'package-lock.json', 'yarn.lock', 'pnpm-lock.yaml', 'bun.lockb', 'bun.lock' }
+        local project_root = vim.fs.root(bufnr, root_markers)
+        if project_root then on_dir(project_root) end
+      end,
       filetypes = {
         'javascript',
         'javascriptreact',
@@ -70,7 +84,6 @@ lsp.init = function()
         'vue',
       },
     },
-    vue_ls = {},
   }
 
   local ensure_installed = vim.tbl_keys(servers or {})
@@ -94,5 +107,36 @@ lsp.init = function()
     })
   end
 end
+
+local function virtual_text_document(params)
+  local bufnr = params.buf
+  local actual_path = params.match:sub(1)
+
+  local clients = vim.lsp.get_clients({ name = 'denols' })
+  if #clients == 0 then return end
+
+  local client = clients[1]
+  local method = 'deno/virtualTextDocument'
+  local req_params = { textDocument = { uri = actual_path } }
+  local response = client.request_sync(method, req_params, 2000, 0)
+  if not response or type(response.result) ~= 'string' then return end
+
+  local lines = vim.split(response.result, '\n')
+  vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, lines)
+  vim.api.nvim_set_option_value('readonly', true, { buf = bufnr })
+  vim.api.nvim_set_option_value('modified', false, { buf = bufnr })
+  vim.api.nvim_set_option_value('modifiable', false, { buf = bufnr })
+  vim.api.nvim_buf_set_name(bufnr, actual_path)
+  vim.lsp.buf_attach_client(bufnr, client.id)
+
+  local filetype = 'typescript'
+  if actual_path:sub(-3) == '.md' then filetype = 'markdown' end
+  vim.api.nvim_set_option_value('filetype', filetype, { buf = bufnr })
+end
+
+vim.api.nvim_create_autocmd({ 'BufReadCmd' }, {
+  pattern = { 'deno:/*' },
+  callback = virtual_text_document,
+})
 
 return lsp
