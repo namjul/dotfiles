e5bb91a4fe1805c96eb236a44e97f7c5c60b27c0
Samuel Hobl <samuel.hobl@gmail.com>
2025-09-17 23:56:52 +0200

    feat(nvim): change autosuggestion keymaps

--- aspects/nvim/files/.config/nvim/plugin/20_plugins.lua
+++ aspects/nvim/files/.config/nvim/plugin/20_plugins.lua
@@ -465,18 +465,12 @@ later(function()
       ['<C-e>'] = { 'hide', 'fallback' },
       ['<CR>'] = { 'accept', 'fallback' },
 
-      ['<Tab>'] = { 'snippet_forward', 'fallback' },
-      ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
+      ['<Tab>'] = { 'accept', 'fallback' },
 
       ['<Up>'] = { 'select_prev', 'fallback' },
       ['<Down>'] = { 'select_next', 'fallback' },
       ['<C-p>'] = { 'select_prev', 'fallback_to_mappings' },
       ['<C-n>'] = { 'select_next', 'fallback_to_mappings' },
-
-      ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
-      ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
-
-      ['<C-k>'] = { 'show_signature', 'hide_signature', 'fallback' },
     },
     sources = {
       providers = {
