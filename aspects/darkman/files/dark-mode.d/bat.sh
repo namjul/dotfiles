d277afbc9eb91e172820f3a06fe5b9485a2aca4d
Samuel Hobl <samuel.hobl@gmail.com>
2025-09-28 22:21:05 +0200

    feat(darkman): add bat

---
+++ aspects/darkman/files/dark-mode.d/bat.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+sed -i 's/gruvbox-light/gruvbox-dark/' ~/.config/bat/config
---
+++ aspects/darkman/files/light-mode.d/bat.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+sed -i 's/gruvbox-dark/gruvbox-light/' ~/.config/bat/config
