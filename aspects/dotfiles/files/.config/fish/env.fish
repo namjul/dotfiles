17931f527a4bbadb00f28c5b657cecd167ca51d6
Samuel Hobl <samuel.hobl@gmail.com>
2026-05-01 16:48:41 +0200

    repair(dotfiles, sops): move env into mise context

--- aspects/dotfiles/files/.config/fish/env.fish
+++ aspects/dotfiles/files/.config/fish/env.fish
@@ -69,6 +69,3 @@ export PKG_CONFIG_PATH="/usr/lib/x86_64-linux-gnu/pkgconfig:/usr/share/pkgconfig
 # Shellbot.
 # For now, requires a manual `cargo build --release`.
 export SHELLBOT="$HOME/.local/share/nvim/site/pack/deps/opt/shellbot/target/release/shellbot $HOME/.config/shellbot/prompt"
-
-# age key
-export SOPS_AGE_KEY_FILE=~/.config/mise/age.txt
