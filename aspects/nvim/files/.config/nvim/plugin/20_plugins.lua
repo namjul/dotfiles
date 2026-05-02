29994538fd541d8e3faf7176809d7f5567188de7
Samuel Hobl <samuel.hobl@gmail.com>
2026-05-02 12:34:21 +0200

    strengthen(nvim): add syntax highlighting to mise config files
    See: https://mise.jdx.dev/mise-cookbook/neovim.html#syntax-highlighting

--- aspects/nvim/files/.config/nvim/plugin/20_plugins.lua
+++ aspects/nvim/files/.config/nvim/plugin/20_plugins.lua
@@ -104,6 +104,12 @@ now_if_args(function()
   })
 
   vim.treesitter.language.register('markdown', 'mdx') -- the someft filetype will use the python parser and queries.
+
+  require('vim.treesitter.query').add_predicate('is-mise?', function(_, _, bufnr, _)
+    local filepath = vim.api.nvim_buf_get_name(tonumber(bufnr) or 0)
+    local filename = vim.fn.fnamemodify(filepath, ':t')
+    return string.match(filename, '.*mise.*%.toml$') ~= nil
+  end, { force = true, all = false })
 end)
 
 now_if_args(function()
