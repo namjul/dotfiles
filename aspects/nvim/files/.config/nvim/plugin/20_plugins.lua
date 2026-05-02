ba3b332e0647f4182576402216560b738f3c2c50
Samuel Hobl <samuel.hobl@gmail.com>
2026-05-02 12:46:20 +0200

    revision(nvim): allow `mise/config.toml` for mise hl

--- aspects/nvim/files/.config/nvim/plugin/20_plugins.lua
+++ aspects/nvim/files/.config/nvim/plugin/20_plugins.lua
@@ -107,8 +107,7 @@ now_if_args(function()
 
   require('vim.treesitter.query').add_predicate('is-mise?', function(_, _, bufnr, _)
     local filepath = vim.api.nvim_buf_get_name(tonumber(bufnr) or 0)
-    local filename = vim.fn.fnamemodify(filepath, ':t')
-    return string.match(filename, '.*mise.*%.toml$') ~= nil
+    return string.match(filepath, '.*mise.*%.toml$') ~= nil
   end, { force = true, all = false })
 end)
 
