44efeb3013457024c7d68e00e9c6679e33ae7ed7
Samuel Hobl <samuel.hobl@gmail.com>
2026-05-09 12:23:34 +0200

    strengthen(fish): add `s` function to copy path to clipboard

--- aspects/dotfiles/files/.config/fish/functions.fish
+++ aspects/dotfiles/files/.config/fish/functions.fish
@@ -114,3 +114,13 @@ function y
   end
   rm -f -- "$tmp"
 end
+
+function s
+    if test -z "$argv[1]"
+        set -l path (string replace -r "^$HOME" "~" (pwd))
+        echo -n $path | pbcopy
+    else
+        set -l path (string replace -r "^$HOME" "~" (realpath $argv[1]))
+        echo -n $path | pbcopy
+    end
+end
