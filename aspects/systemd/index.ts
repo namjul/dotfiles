5579865594c80ef471c353f9e40acfc3d7bcd8cf
Samuel Hobl <samuel.hobl@gmail.com>
2026-05-22 12:14:27 +0200

    refactor(systemd): use fig

---
+++ aspects/systemd/index.ts
@@ -0,0 +1,30 @@
+#!/usr/bin/env -S deno run --allow-all
+
+import { assert, command, file, init, path } from "fig";
+
+init(import.meta.dirname);
+
+const userServices = [
+  ".config/systemd/user/ssh-agent.service",
+  ".config/systemd/user/darkman.service",
+  ".config/systemd/user/redshift.service",
+];
+
+for (const src of userServices) {
+  const r = await file({
+    force: true,
+    path: path.home.join(src),
+    src: path.aspect.join("files", src),
+    state: "link",
+  });
+  assert.result(r);
+}
+
+const reload = await command("systemctl", ["--user", "daemon-reload"]);
+assert.result(reload);
+
+for (const service of ["ssh-agent.service", "darkman.service", "redshift.service"]) {
+  const r = await command("systemctl", ["--user", "enable", "--now", service]);
+  assert.result(r);
+}
+
