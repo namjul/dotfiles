293c29ea3f30d199bab40c5a323f38393a1d1092
Samuel Hobl <samuel.hobl@gmail.com>
2026-05-27 11:42:56 +0200

    strengthen(nvim, conform): option to disable format_on_save

--- aspects/nvim/files/.config/nvim/plugin/20_plugins.lua
+++ aspects/nvim/files/.config/nvim/plugin/20_plugins.lua
@@ -488,7 +488,17 @@ later(function()
 
   require('conform').setup({
     notify_on_error = true,
-    format_on_save = true,
+    format_on_save = function(bufnr)
+      -- Disable autoformat on certain filetypes
+      local ignore_filetypes = {}
+      if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then return end
+      -- Disable with a global or buffer-local variable
+      if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then return end
+      -- Disable autoformat for files in a certain path
+      local bufname = vim.api.nvim_buf_get_name(bufnr)
+      if bufname:match('/node_modules/') then return end
+      return { timeout_ms = 500, lsp_format = 'fallback' }
+    end,
     formatters_by_ft = {
       lua = { 'stylua' },
       python = function(bufnr)
