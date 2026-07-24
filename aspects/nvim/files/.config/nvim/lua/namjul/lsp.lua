37164d0b8203cbc929c108b6f64ef8d7d580102e
Samuel Hobl <samuel.hobl@gmail.com>
2026-07-24 12:23:40 +0200

    strengthen(nvim/lsp): add `aube-lock.yaml` as root marker

--- aspects/nvim/files/.config/nvim/lua/namjul/lsp.lua
+++ aspects/nvim/files/.config/nvim/lua/namjul/lsp.lua
@@ -79,7 +79,14 @@ lsp.init = function()
         },
       },
       root_dir = function(bufnr, on_dir)
-        local root_markers = { 'package-lock.json', 'yarn.lock', 'pnpm-lock.yaml', 'bun.lockb', 'bun.lock' }
+        local root_markers = {
+          'package-lock.json',
+          'yarn.lock',
+          'pnpm-lock.yaml',
+          'bun.lockb',
+          'bun.lock',
+          'aube-lock.yaml',
+        }
         local project_root = vim.fs.root(bufnr, root_markers)
         if project_root then on_dir(project_root) end
       end,
